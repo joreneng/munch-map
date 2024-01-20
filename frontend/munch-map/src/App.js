21233dd7c9fbede572e11dd3747cade87b7dfa89
alecishuman <aasitu@gmail.com>
2024-01-20 12:52:24 -0800

    frontend setup done

--- frontend/munch-map/src/App.js
+++ frontend/munch-map/src/App.js
@@ -5,7 +5,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Hello world!
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
