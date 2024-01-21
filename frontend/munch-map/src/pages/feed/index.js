aa26ab2f177c6106bb9c0c6ba49751adadab04f5
tsomani <tabreek.somani@gmail.com>
2024-01-20 19:50:40 -0800

    Updated feed

--- frontend/munch-map/src/pages/feed/index.js
+++ frontend/munch-map/src/pages/feed/index.js
@@ -1,11 +1,30 @@
 import FoodItem from "../../components/food_item";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import SearchBar from "../../components/search-bar";
 import "./index.css";
 import Food from "../../components/food";
 
 export default function Feed() {
+
+  const [food, setFood] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:8080/available", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    setFood(data);
+
+  };
+  useEffect(() => {
+   
+    fetchData();
+  }, []);
   const [search, setSearch] = useState("");
   const feedData = [
     {
@@ -49,20 +68,35 @@ export default function Feed() {
       )}
 
       <div className="w-full flex flex-col items-center mt-3">
-        {feedData
-          .filter((item, key) =>
-            search
-              ? item.name.toLowerCase().includes(search.toLowerCase())
-              : key != 0
-          )
-          .map((item) => (
-            <FoodItem
-              name={item.name}
-              expiry={item.expiry}
-              vegan={item.vegan}
-              vegetarian={item.vegetarian}
-            />
-          ))}
+        {
+          food
+            .filter((item, key) =>
+              search
+                ? item.name.toLowerCase().includes(search.toLowerCase())
+                : key != 0
+            )
+            .map((item) => {
+              const expiryDate = new Date(item.expiry);
+              const currentDate = new Date();
+              const diffTime = Math.abs(expiryDate - currentDate);
+              const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+              return (
+                <FoodItem
+                  id={item.id}
+                  name={item.name}
+                  expiry={diffDays}
+                  image={item.image}
+                  location={item.address}
+                  type={item.type}
+                  description={item.description}
+                  vegan={item.vegan}
+                  vegetarian={item.vegetarian}
+                  orderText={"Order"}
+                />
+              );
+            })
+        }
       </div>
     </div>
   );
