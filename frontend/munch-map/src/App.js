abb9931f68f86105ec5dcdacb9a5bcfabcb24633
alecishuman <aasitu@gmail.com>
2024-01-20 17:18:51 -0800

    feed page

--- frontend/munch-map/src/App.js
+++ frontend/munch-map/src/App.js
@@ -3,13 +3,14 @@ import Home from "./pages/home";
 import "./App.css";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
+import Feed from "./pages/feed";
 
 function App() {
   return (
     <div className="App flex justify-center items-center">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Feed />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
